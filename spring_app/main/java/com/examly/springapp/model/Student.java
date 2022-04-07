f41554af47b5e00d0d92dd3b7ee9e0a5388b4e79
manasa54 <75627414+manasa54@users.noreply.github.com>
2022-04-07 21:00:57 +0530

    Add files via upload

---
+++ spring_app/main/java/com/examly/springapp/model/Student.java
@@ -0,0 +1,80 @@
+package com.examly.springapp.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String password;
+    private String username;
+    private String Mobile_number;
+    private int age;
+    private String userRole;
+    public Student()
+    {
+
+    }
+
+    public Student(String email, String password, String username, String mobile_number, int age, String userRole) {
+        super();
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        Mobile_number = mobile_number;
+        this.age = age;
+        this.userRole = userRole;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobile_number() {
+        return Mobile_number;
+    }
+
+    public void setMobile_number(String mobile_number) {
+        Mobile_number = mobile_number;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
---
+++ spring_app/main/java/com/examly/springapp/model/login.java
@@ -0,0 +1,42 @@
+package com.examly.springapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String password;
+
+    public login()
+    {
+
+    }
+
+    public login(String email, String password) {
+        super();
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
